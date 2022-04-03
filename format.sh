4401d864192c8f55959788c6db90d6180475e442
DillonB07 <>
2022-04-03 16:06:50 +0000

    Add finishing layout touches

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 prettier index.html style.css -w
-yarn postcss *.css --use autoprefixer -d build/
-prettier index.html style.css build/style.css -w
+yarn postcss *.css --use autoprefixer -d ./
+prettier index.html style.css -w
